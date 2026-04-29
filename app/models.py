deeb977f3f12dd2442cd0ca512514b9e36e6edbd
Mo_PC <mohamedazahrioui2006@gmail.com>
2026-04-29 15:21:58 +0200

    Add User model for authentication

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
-from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy import Column, Float, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
 
 class Invoice(Base):
